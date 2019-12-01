aedf58f527be2a55cf5d69b5480fdfaeca31f2df
Usama Tariq <44930673+usama-tariq1@users.noreply.github.com>
2019-12-01 00:50:15 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,3 @@
-$(function(){
-    // don't cache ajax or content won't be fresh
-    // $.ajaxSetup ({
-    //     cache: false
-    // });
-    $.ajaxSetup({
-        cache : false
-    });
-    var ajax_load = "<img src='icons/preloader3.gif' style='height:50px; display:block; margin:0 auto;' alt='loading...' />";
-    
-    // load() functions
-    var loadUrl = "http://localhost/logico/content.php";
-    $("#result").html(ajax_load).load(loadUrl);
-
-    $("#loadbasic").click(function(){
-        $("#result").html(ajax_load).load(loadUrl);
-    });
-
-    // function load(){
-    //     document.getElementById()
-    // }
-
-// end  
-});
-
 
 
 $(document).on('click','#save',function(e) {
@@ -31,39 +6,16 @@ $(document).on('click','#save',function(e) {
            data: data,
            type: "post",
            url: "db/addtrans.php",
-        //    success: function(data){
-        //     //    reload();
-        //     // alert ("New Transaction: " +data);
+           success: function(data){
+               reload();
+               alert ("New Transaction: " +data);
                 
-        //    }
-  });
-    cload();
-   });
+           }
+});
+
+});
 
 
 
-function cload(){
-    var ajax_load = "<img src='icons/preloader3.gif' style='height:50px; display:block; margin:0 auto;' alt='loading...' />";
-    
-    // load() functions
-    var loadUrl = "http://localhost/logico/content.php";
-    
-    $("#result").html(ajax_load).load(loadUrl);
-}
 
 
-function deletet(id){
-    var data = $(id).serialize();
-    $.ajax({
-           data: data,
-           type: "post",
-           url: "db/deletet.php",
-        //    success: function(data){
-        //     //    reload();
-        //         // alert ("action: " + data);
-                
-        //    }
-  });
-    cload();
-
-}
